db73e396a6484cdbf4db9bfff84f1dcfa4fc10c7
Matt Rowan <matt.rowan121@gmail.com>
2023-04-18 18:44:40 -0400

    Add article container and component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { getArticles } from "../../Utilities/apiCalls";
+import Article from "../Article";
 
 const App = () => {
 
   const [articles, setArticles] = useState([])
 
   useEffect(() => {
-    getArticles("movies")
+    getArticles("politics")
     .then(data => setArticles(data.results))
-    // .catch(error => this.setState({ error: `${error}`}))
+  })
+
+  const articleDisplay = articles.map(obj => {
+    return (
+      <Article 
+        image={obj.multimedia[0].url} 
+        caption={obj.multimedia[0].caption} 
+        title={obj.title} 
+        abstract={obj.abstract} 
+        key={obj.title}
+        url={obj.url}
+      />
+    )
   })
 
   return (
     <div className="">
       <header className=""></header>
       <h1>Test</h1>
+      <div className="bg-gray-700 text-gray-200 p-5 place-items-center gap-4 select-none grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+        {articleDisplay}
+      </div>
     </div>
   );
 }
