7e8872809ad50c85f2309dde790a2bfca6e280ca
Matt Rowan <matt.rowan121@gmail.com>
2023-04-18 19:53:48 -0400

    Add list elements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,8 +2,20 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <>
-        </>
+        <ul>
+            <li>Home</li>
+            <li>Arts</li>
+            <li>Business</li>
+            <li>Health</li>
+            <li>Home</li>
+            <li>Movies</li>
+            <li>Politics</li>
+            <li>Science</li>
+            <li>Sports</li>
+            <li>Technology</li>
+            <li>Travel</li>
+            <li>World</li>
+        </ul>
     )
 }
 
