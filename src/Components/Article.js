81436df5b2e55d7455f8c424888c33e922171282
Matt Rowan <matt.rowan121@gmail.com>
2023-04-18 19:04:11 -0400

    Change class to className

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Article = ({ image, caption, title, abstract, url }) => {
     return (
-        <div class="max-w-sm border rounded-lg shadow bg-gray-800 border-gray-700">
-            <img class="rounded-t-lg object-cover h-52 w-full" src={image} alt={caption}/>
-            <div class="p-5 h-72">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">{title}</h5>
-                <p class="mb-3 font-normal text-gray-400 line-clamp-5">{abstract}</p>
+        <div className="max-w-sm border rounded-lg shadow bg-gray-800 border-gray-700">
+            <img className="rounded-t-lg object-cover h-52 w-full" src={image} alt={caption}/>
+            <div className="p-5 h-72">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{title}</h5>
+                <p className="mb-3 font-normal text-gray-400 line-clamp-5">{abstract}</p>
             </div>
             <a href={url}>
                 <button 
