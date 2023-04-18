296f41cf102beb779d3fc8b8f593272b43b822c6
Matt Rowan <matt.rowan121@gmail.com>
2023-04-18 15:40:10 -0400

    Import api call and create fetch request

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { getArticles } from "../../Utilities/apiCalls";
 
-function App() {
+const App = () => {
+
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    getArticles("movies")
+    .then(data => setArticles(data.results))
+    // .catch(error => this.setState({ error: `${error}`}))
+  })
+
   return (
     <div className="">
       <header className=""></header>
