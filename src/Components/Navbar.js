46817bcc69f2f22d7d9a6ac643f84a881518d109
Matt Rowan <matt.rowan121@gmail.com>
2023-04-18 20:12:09 -0400

    Convert list elements to links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <ul className="flex justify-center space-x-4 cursor-pointer bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
-            <li>Home</li>
-            <li>Arts</li>
-            <li>Business</li>
-            <li>Health</li>
-            <li>Home</li>
-            <li>Movies</li>
-            <li>Politics</li>
-            <li>Science</li>
-            <li>Sports</li>
-            <li>Technology</li>
-            <li>Travel</li>
-            <li>World</li>
+            <Link to='/home'>Home</Link>
+            <Link to='/arts'>Arts</Link>
+            <Link to='/business'>Business</Link>
+            <Link to='/health'>Health</Link>
+            <Link to='/movies'>Movies</Link>
+            <Link to='/politics'>Politics</Link>
+            <Link to='/science'>Science</Link>
+            <Link to='/sports'>Sports</Link>
+            <Link to='/technology'>Technology</Link>
+            <Link to='/travel'>Travel</Link>
+            <Link to='/world'>World</Link>
         </ul>
     )
 }
