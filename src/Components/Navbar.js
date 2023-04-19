aedd386150c083a688a5e2f3a24809f7f0ab3a9c
Matt Rowan <matt.rowan121@gmail.com>
2023-04-18 20:05:20 -0400

    Add initial styling to nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <ul>
+        <ul className="flex justify-center space-x-4 cursor-pointer bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
             <li>Home</li>
             <li>Arts</li>
             <li>Business</li>
