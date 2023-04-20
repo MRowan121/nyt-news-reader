0f4d2c599eeb9471a3bf0901c44bfea1e3bb27e0
Matt Rowan <matt.rowan121@gmail.com>
2023-04-20 11:27:15 -0400

    Remove several links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
     return (
-        <ul className="flex justify-center space-x-4 cursor-pointer bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
-            <Link to='/home'>Home</Link>
-            <Link to='/arts'>Arts</Link>
-            <Link to='/business'>Business</Link>
-            <Link to='/health'>Health</Link>
-            <Link to='/movies'>Movies</Link>
-            <Link to='/politics'>Politics</Link>
-            <Link to='/science'>Science</Link>
-            <Link to='/sports'>Sports</Link>
-            <Link to='/technology'>Technology</Link>
-            <Link to='/travel'>Travel</Link>
-            <Link to='/world'>World</Link>
+        <ul className="flex justify-center space-x-4 cursor-pointer sticky top-0 bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
+            <Link to='/articles/home'>Home</Link>
+            <Link to='/articles/business'>Business</Link>
+            <Link to='/articles/movies'>Movies</Link>
+            <Link to='/articles/politics'>Politics</Link>
+            <Link to='/articles/sports'>Sports</Link>
+            <Link to='/articles/technology'>Technology</Link>
+            <Link to='/articles/world'>World</Link>
         </ul>
     )
 }
