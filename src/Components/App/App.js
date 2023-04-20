0cd849d01a41f0c1b3031fc80232aea723f46daa
Matt Rowan <matt.rowan121@gmail.com>
2023-04-20 11:28:01 -0400

    Create use effect to store url location

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from "react";
 import { getArticles } from "../../Utilities/apiCalls";
 import Navbar from "../Navbar";
 import ArticleContainer from "../ArticleContainer";
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
 
-const App = () => {
+function App() {
 
   const [articles, setArticles] = useState([])
+  const [locationPath, setPath] = useState("home")
+  
+  const location = useLocation()
 
   useEffect(() => {
-    getArticles("politics")
+      const category = location.pathname.split("/")[2]
+      setPath(category)
+  }, [location.pathname])
+ 
+  useEffect(() => {
+    getArticles(locationPath)
     .then(data => {
       const manipulatedData = data.results.map(obj => {
         const searchData = [obj.des_facet, obj.org_facet, obj.per_facet, obj.geo_facet]
@@ -16,12 +25,16 @@ const App = () => {
       })
       setArticles(manipulatedData)
     })
-  })
+  }, [articles, locationPath])
 
   return (
     <div className="">
       <Navbar />
-      <ArticleContainer articles={articles} />
+      <Routes>
+        {/* <Route path='/' element={<ArticleContainer articles={articles} />} /> */}
+        <Route path={`/articles/:selection`} element={<ArticleContainer articles={articles} />} />
+        <Route path="*" element={<Navigate to={`/articles/${locationPath}`} replace />} />
+      </Routes>
     </div>
   );
 }
